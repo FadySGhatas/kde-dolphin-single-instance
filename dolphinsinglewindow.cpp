1e302068046a425fa8861f8574659468ca64efec
FadySGhatas <fady@titrias.com>
2018-05-16 20:17:17 +0200

    Update dolphinsinglewindow.cpp

--- dolphinsinglewindow.cpp
+++ dolphinsinglewindow.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
-*   Copyright (C) 2016 by Fady S. Ghatas <fady@titrias.com>               *
+*   Copyright (C) 2016 by Fady S. Ghatas @ TiTrias                        *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
 *   it under the terms of the GNU General Public License as published by  *
